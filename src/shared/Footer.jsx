2d0e7f844866c84f79b9065b1104c6dd3fc6079e
Masum Rahman <masumrahman73@gmail.com>
2023-07-22 23:37:47 +0600

    footer chamged

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="mt-4 flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
             <Typography color="blue-gray" className="font-normal">
-                &copy; 2023 Material Tailwind
+                &copy; 2023 Masum Rahman
             </Typography>
             <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                 <li>
